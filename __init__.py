6ece16c3d673773384d36414ddce3fea3b0d343e
Vladimir Still <git@vstill.eu>
2020-09-26 21:18:21 +0200

    turn into installable package

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__title__ = 'feedie'
-__version__ = '1.1'
-__author__ = 'meigrafd <meiraspi@gmail.com>, Vladimír Štill <feedie@vstill.eu>'
-__repo__ = 'https://github.com/vlstill/feedie'
-__license__ = 'Creative Commons License (BY-NC-SA)'
