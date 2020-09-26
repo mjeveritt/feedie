22a0bae5d911adaf881b3e11660263d0cdbf48a1
Vladimir Still <git@vstill.eu>
2020-09-26 15:36:14 +0200

    update metadata to the fork

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'feedie'
-__version__ = '1.0'
-__author__ = 'meigrafd <meiraspi@gmail.com>'
-__repo__ = 'https://github.com/meigrafd/feedie'
+__version__ = '1.1'
+__author__ = 'meigrafd <meiraspi@gmail.com>, Vladimír Štill <feedie@vstill.eu>'
+__repo__ = 'https://github.com/vlstill/feedie'
 __license__ = 'Creative Commons License (BY-NC-SA)'
